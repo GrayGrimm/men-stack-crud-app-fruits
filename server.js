b06001436edbc186bcad5ec14438bdb061d14e73
guyera <thegraygrimm@graymansurvivaltech.com>
2025-07-18 12:44:24 -0400

    Created route for ejs

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 const express = require('express');
 const app = express()
 
+//get route to slash
+app.get('/', async (req, res) => {
+    // res.send('hello,friend') add these lines throughough out the process to check the get route
+    res.render('index.ejs');
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
